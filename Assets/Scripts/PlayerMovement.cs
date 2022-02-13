20b3483fd093acb7457e8564dd2c16ca1e74b5f2
Ikamjot Hundal <ihundal@my.centennialcollege.ca>
2022-02-13 16:44:02 -0500

    Added in the velocity of the gravity

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,13 +11,29 @@ public class PlayerMovement : MonoBehaviour
     // reference to the Character Controller
     public CharacterController characterController;
 
-    // backends variables 
-    [SerializeField] private float speed = 12f; 
+    // GroundCheck variables
+    public Transform groundCheck; 
+    [SerializeField] private float groundDistance = 0.5f;
+    public LayerMask groundMask;
+    private bool isGrounded; 
 
+    // backends variables 
+    [SerializeField] private float speed = 12f;
+
+    // gravity variables 
+    Vector3 velocity;
+    [SerializeField] private float gravity = -9.81f; 
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); 
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         // assigning it to the axes 
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
@@ -30,11 +46,18 @@ public class PlayerMovement : MonoBehaviour
          Vector3 move = new Vector3(horizontalInput, 0f, verticalInput);
         */
 
-
+        // getting the local coords
         Vector3 movement = transform.right * horizontalInput + transform.forward * verticalInput;
 
+        // move the character using character controller 
+        characterController.Move(movement * speed * Time.fixedDeltaTime);
 
-        characterController.Move(movement * speed * Time.fixedDeltaTime); 
+
+        // setting up gravity 
+
+        velocity.y += gravity * Time.deltaTime;
+
+        characterController.Move(velocity * Time.deltaTime); 
 
 
     }
