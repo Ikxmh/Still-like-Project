1ad0241c33e86238d07b549a33e54f243b05a443
Ikamjot Hundal <ihundal@my.centennialcollege.ca>
2022-02-13 16:22:32 -0500

    Fixed the camera limits & added movement

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -30,7 +30,7 @@ public class MouseLook : MonoBehaviour
 
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 40f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Inputs variables 
+    private float horizontalInput;
+    private float verticalInput;
+
+    // reference to the Character Controller
+    public CharacterController characterController;
+
+    // backends variables 
+    [SerializeField] private float speed = 12f; 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // assigning it to the axes 
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // movement 
+
+        /*
+         * global coordinates 
+         * (would move same way no matter what way our player is facing)
+         Vector3 move = new Vector3(horizontalInput, 0f, verticalInput);
+        */
+
+
+        Vector3 movement = transform.right * horizontalInput + transform.forward * verticalInput;
+
+
+        characterController.Move(movement * speed * Time.fixedDeltaTime); 
+
+
+    }
+}
